841da3a18873a6101f8c8a42ea1d7c9843c41b4c
anjum2188 <123760870+anjum2188@users.noreply.github.com>
2023-12-05 16:02:22 +0800

    Update mysqlsampledatabase.sql

--- mysqlsampledatabase.sql
+++ mysqlsampledatabase.sql
@@ -1,8 +1,3 @@
-/*
-  Name: MySQL Sample Database classicmodels
-  Link: http://www.mysqltutorial.org/mysql-sample-database.aspx
-*/
-
 
 /* Create the database */
 CREATE DATABASE  IF NOT EXISTS classicmodels;
